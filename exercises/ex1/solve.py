15bbad746e7a8216b4848c8fe3de9fc41f3cb30c
mralinp <alinaderiparizi@yahoo.com>
2020-11-11 10:11:35 +0330

    done!

--- exercises/ex1/solve.py
+++ exercises/ex1/solve.py
@@ -1,7 +1,7 @@
-#! /usr/bin/python3
 import cv2 as cv
 import numpy as np
 import math
+
 
 def show(name, img):
     cv.namedWindow(name)
@@ -18,36 +18,38 @@ def firstStep():
     show("peppers", pepersImg)
     return einsteinImg, pepersImg
 
+
 def secondStep(img1, img2):
     height, width = img2.shape
     newImgMat = np.zeros((height, width), np.uint8)
     for i in range(width):
         for j in range(height):
-                newImgMat[j][i] = img2[j][i] if (i < 127) else img1[j][i]
+            newImgMat[j][i] = img2[j][i] if (i < 127) else img1[j][i]
     show("peptein", newImgMat)
     cv.imwrite("./peptein.jpg", newImgMat)
-    return newImgMat    
+    return newImgMat
+
 
 def thiredStep(J):
     J_neg = 255 - J
     show("J-neg", J_neg)
-    cv.imwrite("negative_peppers.jpg", J_neg)
+    cv.imwrite("negative_einstein.jpg", J_neg)
+
 
 def forthStep():
     coloredPeppers = cv.imread("peppers_color.png")
     height, width, channels = coloredPeppers.shape
-    blue_peppers = coloredPeppers[:,:,0]
-    green_peppers = coloredPeppers[:,:,1]
-    red_peppers  = coloredPeppers[:,:,2]
+    blue_peppers = coloredPeppers[:, :, 0]
+    green_peppers = coloredPeppers[:, :, 1]
+    red_peppers = coloredPeppers[:, :, 2]
     show("blue-channel", blue_peppers)
     show("green-channel", green_peppers)
     show("red-channel", red_peppers)
 
-    
 
 if __name__ == "__main__":
     enistein, peppers = firstStep()
     J = secondStep(enistein, peppers)
     thiredStep(J)
     forthStep()
-    print ("Done!")
+    print("Done!")
